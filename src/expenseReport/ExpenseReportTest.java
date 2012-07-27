386f9dccea1aee753dbe9a71d9c151b810a03454
Sandro Mancuso <sandromancuso@londonswcraft.com>
2012-07-27 01:31:28 +0100

    Duplication removed and improvements in readability

--- src/expenseReport/ExpenseReportTest.java
+++ src/expenseReport/ExpenseReportTest.java
@@ -1,9 +1,10 @@
 package expenseReport;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class ExpenseReportTest {
   private ExpenseReport report;
@@ -17,85 +18,101 @@ public class ExpenseReportTest {
     printer = new MockReportPrinter();
   }
 
-  @Test
-  public void printEmpty() {
+  @Test public void 
+  should_print_empty_report() {
     reporter.printReport(printer);
 
-    assertEquals(
-      "Expenses 9/12/2002\n" +
-        "\n" +
-        "Meal expenses $0.00\n" +
-        "Total $0.00",
-      printer.getText());
+    assertThat(printer.getText(), is(
+    							   	  "Expenses 9/12/2002\n" +
+    							   	  "\n" +
+    							      "Meal expenses $0.00\n" +
+    								  "Total $0.00"));
   }
 
-  @Test
-  public void printOneDinner() {
-    report.addExpense(new DinnerExpense(1678));
+  @Test public void 
+  should_print_one_dinner() {
+    givenReportHas(dinnerCosting(1678));
+    
     reporter.printReport(printer);
 
-    assertEquals(
-      "Expenses 9/12/2002\n" +
-        " \tDinner\t$16.78\n" +
-        "\n" +
-        "Meal expenses $16.78\n" +
-        "Total $16.78",
-      printer.getText());
+    assertThat(printer.getText(), is(
+								      "Expenses 9/12/2002\n" +
+								      " \tDinner\t$16.78\n" +
+								      "\n" +
+								      "Meal expenses $16.78\n" +
+								      "Total $16.78"));
   }
 
-  @Test
-  public void twoMeals() throws Exception {
-    report.addExpense(new DinnerExpense(1000));
-    report.addExpense(new BreakfastExpense(500));
-    reporter.printReport(printer);
+  @Test public void 
+  should_print_two_meals() throws Exception {
+	givenReportHas(dinnerCosting(1000), 
+				   breakfastCosting(500));
+    
+	reporter.printReport(printer);
 
-    assertEquals(
-      "Expenses 9/12/2002\n" +
-        " \tDinner\t$10.00\n" +
-        " \tBreakfast\t$5.00\n" +
-
-        "\n" +
-        "Meal expenses $15.00\n" +
-        "Total $15.00",
-      printer.getText());
+    assertThat(printer.getText(), is(
+								      "Expenses 9/12/2002\n" +
+								      " \tDinner\t$10.00\n" +
+								      " \tBreakfast\t$5.00\n" +
+								      "\n" +
+								      "Meal expenses $15.00\n" +
+								      "Total $15.00"));
   }
 
-  @Test
-  public void twoMealsAndCarRental() throws Exception {
-    report.addExpense(new DinnerExpense(1000));
-    report.addExpense(new BreakfastExpense(500));
-    report.addExpense(new CarRentalExpense(50000));
+  @Test public void 
+  should_print_two_meals_and_a_car_rental() throws Exception {
+    givenReportHas(dinnerCosting(1000),
+    		       breakfastCosting(500),
+    		       carRentalCosting(50000));
+    
     reporter.printReport(printer);
 
-    assertEquals(
-      "Expenses 9/12/2002\n" +
-        " \tDinner\t$10.00\n" +
-        " \tBreakfast\t$5.00\n" +
-        " \tCar Rental\t$500.00\n" +
-        "\n" +
-        "Meal expenses $15.00\n" +
-        "Total $515.00",
-      printer.getText());
+    assertThat(printer.getText(), is(
+								      "Expenses 9/12/2002\n" +
+								      " \tDinner\t$10.00\n" +
+								      " \tBreakfast\t$5.00\n" +
+								      " \tCar Rental\t$500.00\n" +
+								      "\n" +
+								      "Meal expenses $15.00\n" +
+								      "Total $515.00"));
   }
 
-  @Test
-  public void overages() throws Exception {
-    report.addExpense(new BreakfastExpense(1000));
-    report.addExpense(new BreakfastExpense(1001));
-    report.addExpense(new DinnerExpense(5000));
-    report.addExpense(new DinnerExpense(5001));
+  @Test public void 
+  should_print_overages() throws Exception {
+    givenReportHas(breakfastCosting(1000),
+    		       breakfastCosting(1001),
+    		       dinnerCosting(5000),
+    		       dinnerCosting(5001));
+    
     reporter.printReport(printer);
 
-    assertEquals(
-      "Expenses 9/12/2002\n" +
-        " \tBreakfast\t$10.00\n" +
-        "X\tBreakfast\t$10.01\n" +
-        " \tDinner\t$50.00\n" +
-        "X\tDinner\t$50.01\n" +
-        "\n" +
-        "Meal expenses $120.02\n" +
-        "Total $120.02",
-      printer.getText());
+    assertThat(printer.getText(), is(
+								      "Expenses 9/12/2002\n" +
+								      " \tBreakfast\t$10.00\n" +
+								      "X\tBreakfast\t$10.01\n" +
+								      " \tDinner\t$50.00\n" +
+								      "X\tDinner\t$50.01\n" +
+								      "\n" +
+								      "Meal expenses $120.02\n" +
+								      "Total $120.02"));
+  }
+
+  private DinnerExpense dinnerCosting(int amount) {
+	return new DinnerExpense(amount);
+  }
+
+  private BreakfastExpense breakfastCosting(int amount) {
+	return new BreakfastExpense(amount);
+  }
+  
+  private CarRentalExpense carRentalCosting(int amount) {
+		return new CarRentalExpense(amount);
+	}  
+
+  private void givenReportHas(Expense... expenses) {
+	for (Expense expense : expenses) {
+		report.addExpense(expense);
+	}
   }
 
 }
